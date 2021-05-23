318941dd0cfbea4b03bb4b967015ade77c251c13
joechamberlain1 <joechamberlain2801@gmail.com>
2021-05-23 22:29:11 +1200

    added tailwind and changed the look

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
+import { CircularProgress } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import Weather from './Components/Weather';
-import './App.css'
+import Weather from "./Components/Weather";
 
 export default function App() {
-  
   const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lon, setLong] = useState([]);
+  // const [cityName, setCityName] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
+      navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
+        console.log(lon, lat);
       });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${lon}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
     fetchData();
-  }, [lat,long])
-
+  }, [lat, lon]);
 
   return (
-    <div className="App">
-      {(typeof data.main != 'undefined')? (
-        <Weather className='weather' weatherData = {data} />
-      ):(
-        <div></div>
-      )}
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen bg-gray-200 uppercase">
+        {typeof data.main != "undefined" ? (
+          <Weather weatherData={data} />
+        ) : (
+          <div className="text-red-800 flex flex-col leading-10 justify-center items-center text-center">
+            <CircularProgress />
+            <p>If nothing happens after 10 seconds please refresh</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import moment from 'moment'
-import './Weather.css'
+import React from "react";
+import moment from "moment";
 
-const Weather = ({weatherData}) => {
-    return (
-        <div className='weather-card'>
-            <div className='card-header'>
-                {weatherData.name}
-            </div>
-            <div className='card-content'>
-                <div className='flex'>
-                <p>Temperature: {weatherData.main.temp}&deg;C</p>
-                <p>Feels like: {weatherData.main.feels_like}&deg;C</p>
-                </div>
-                <div>
-                <p>Sunrise: {new Date(weatherData.sys.sunrise*1000).toLocaleTimeString('en-NZ')}</p>
-                <p>Sunset: {new Date(weatherData.sys.sunset*1000).toLocaleTimeString('en-NZ')}</p>
-                </div>
-                <p>Description: {weatherData.weather[0].description}</p>
-                <p>Date: {moment().format('LL')}</p>
-            </div>
+const Weather = ({ weatherData }) => {
+  return (
+    <div
+      className="flex flex-col lg:text-7xl md:text-5xl sm:text-4xl text-3xl lg: leading-loose shadow-2xl border-solid border-black border-8 p-10 bg-white rounded-3xl
+    "
+    >
+      <div className="text-center pb-10 font-bold tracking-wider text-red-400 hover:underline hover: cursor-pointer">
+        {weatherData.name}
+      </div>
+      <div>
+        <div>
+          <p>Temperature: {weatherData.main.temp}&deg;C</p>
+          <p>Feels like: {weatherData.main.feels_like}&deg;C</p>
         </div>
-    )
-}
+        <div>
+          <p>
+            Sunrise:{" "}
+            {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
+              "en-NZ"
+            )}
+          </p>
+          <p>
+            Sunset:{" "}
+            {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
+              "en-NZ"
+            )}
+          </p>
+        </div>
+        <p>Description: {weatherData.weather[0].description}</p>
+        <p>Date: {moment().format("LL")}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Weather
+export default Weather;
